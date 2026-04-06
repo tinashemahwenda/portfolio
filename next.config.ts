730728a1f23261b8178034e9bcd7e17b18c59f4c
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2026-04-06 20:33:01 +0200

    Config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/portfolio', // Replace with your actual repo name
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
