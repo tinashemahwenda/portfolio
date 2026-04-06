20ae90eb2cf482612dc322510e12b7bb36d22896
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2026-04-06 20:54:19 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/portfolio', // Replace with your actual repo name
-  assetPrefix: '/portfolio/',
+  images: {
+    unoptimized: true,
+  },
+  // ADD THIS LINE: It tells Next.js to append /portfolio to all internal Next.js links and <Image> components
+  basePath: '/portfolio',
 };
+
 
 export default nextConfig;
